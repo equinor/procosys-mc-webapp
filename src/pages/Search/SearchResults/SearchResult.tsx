c23896ae8015fca4eb02819eb07acdf2ce32fbfd
VRiveland <vriv@equinor.com>
2021-04-30 11:39:17 +0200

    Added SearchResult component

---
+++ src/pages/Search/SearchResults/SearchResult.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import styled from 'styled-components';
+import { COLORS } from '../../../style/GlobalStyles';
+import { McPkgPreview } from '../../../services/apiTypes';
+import { SearchType } from '../Search';
+import { McPackageStatusIcon } from '../../../components/icons/McPackageStatusIcon';
+import { Typography } from '@equinor/eds-core-react';
+import { useHistory } from 'react-router';
+
+const SearchResultWrapper = styled.article`
+    cursor: pointer;
+    display: flex;
+    align-items: flex-start;
+    border-top: 1px solid ${COLORS.lightGrey};
+    padding: 12px;
+    margin: 0;
+    &:hover {
+        opacity: 0.7;
+    }
+`;
+
+const StatusImageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-right: 12px;
+    & > img {
+        height: 20px;
+    }
+`;
+
+const StatusTextWrapper = styled.div`
+    display: flex;
+    & > p {
+        margin: 0;
+    }
+`;
+
+const HandoverStatus = styled.p<{ accepted: boolean }>`
+    font-weight: ${(props): string => (props.accepted ? 'bolder' : 'normal')};
+`;
+
+const SearchResultDetailsWrapper = styled.div`
+    flex-direction: column;
+    flex: 1;
+    & > p {
+        margin-bottom: 0;
+        margin-top: 0;
+    }
+`;
+
+const SearchResultHeaderWrapper = styled.div`
+    display: flex;
+    flex: 1;
+    align-items: baseline;
+    margin-bottom: 6;
+    & > h6 {
+        margin: 0;
+        flex: 1.3; // TODO: make this text the correct color blue
+    }
+    & p {
+        margin: 0;
+        flex: 1;
+        text-align: center;
+    }
+`;
+
+type SearchResultProps = {
+    searchType: SearchType;
+    searchResult: McPkgPreview; // TODO: add other types once made
+};
+
+const SearchResult = ({
+    searchResult,
+    searchType,
+}: SearchResultProps): JSX.Element => {
+    const history = useHistory();
+
+    if (searchType === SearchType.MC) {
+        return (
+            <SearchResultWrapper
+                onClick={(): void => history.push(`MC/${searchResult.id}`)}
+                key={searchResult.id}
+            >
+                <StatusImageWrapper>
+                    <McPackageStatusIcon status={searchResult.status} />
+                    <StatusTextWrapper>
+                        <HandoverStatus
+                            accepted={
+                                searchResult.commissioningHandoverStatus ==
+                                'ACCEPTED'
+                            }
+                        >
+                            C
+                        </HandoverStatus>
+                        <HandoverStatus
+                            accepted={
+                                searchResult.operationHandoverStatus ==
+                                'ACCEPTED'
+                            }
+                        >
+                            O
+                        </HandoverStatus>
+                    </StatusTextWrapper>
+                </StatusImageWrapper>
+                <SearchResultDetailsWrapper>
+                    <SearchResultHeaderWrapper>
+                        <h6>{searchResult.mcPkgNo}</h6>
+                        <Typography variant="caption">
+                            {searchResult.commPkgNo}
+                        </Typography>
+                        <Typography variant="caption">
+                            {searchResult.responsibleCode}
+                        </Typography>
+                    </SearchResultHeaderWrapper>
+                    <Typography variant="caption">
+                        {searchResult.description}
+                    </Typography>
+                    <Typography variant="caption">
+                        {searchResult.phaseCode}
+                    </Typography>
+                </SearchResultDetailsWrapper>
+            </SearchResultWrapper>
+        );
+    } else {
+        return <></>;
+    }
+};
+
+export default SearchResult;
--- src/pages/Search/SearchResults/SearchResults.tsx
+++ src/pages/Search/SearchResults/SearchResults.tsx
@@ -8,66 +8,15 @@ import { McPackageStatusIcon } from '../../../components/icons/McPackageStatusIc
 import { COLORS } from '../../../style/GlobalStyles';
 import Search, { SearchType } from '../Search';
 import useCommonHooks from '../../../utils/useCommonHooks';
-
-const SearchResult = styled.article`
-    cursor: pointer;
-    display: flex;
-    align-items: flex-start;
-    border-top: 1px solid ${COLORS.lightGrey};
-    padding: 12px;
-    margin: 0;
-    &:hover {
-        opacity: 0.7;
-    }
-`;
-
-const StatusImageWrapper = styled.div`
-    display: flex;
-    padding-right: 12px;
-    & img {
-        height: 20px;
-    }
-`;
-
-const SearchResultDetailsWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    background-color: red;
-    & p {
-        margin-bottom: 0;
-        margin-top: 0;
-    }
-`;
-
-const SearchResultHeaderWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    align-items: center;
-    background-color: blue;
-    margin-bottom: 6;
-    & h6 {
-        margin: 0;
-        flex: 1.5;
-        background-color: yellow;
-    }
-    & p {
-        margin: 0;
-        flex: 1;
-        text-align: center;
-    }
-`;
+import { accessible } from '@equinor/eds-icons';
+import { Typography } from '@equinor/eds-core-react';
+import SearchResult from './SearchResult';
 
 type SearchResultsProps = {
     searchStatus: SearchStatus;
     searchResults: McPkgPreview[]; // TODO: add other previews as props
     searchType: SearchType;
 };
-
-{
-    // TODO: change to work for all search types
-}
 
 const SearchResults = ({
     searchStatus,
@@ -76,48 +25,19 @@ const SearchResults = ({
 }: SearchResultsProps): JSX.Element => {
     const history = useHistory();
 
-    const determineSearchResultType = (): JSX.Element => {
-        return (
-            <div>
-                {searchResults.map((searchResult) => {
-                    if (searchType === SearchType.MC) {
-                        return (
-                            <SearchResult
-                                onClick={(): void =>
-                                    history.push(`MC/${searchResult.id}`)
-                                }
-                                key={searchResult.id}
-                            >
-                                <StatusImageWrapper>
-                                    <McPackageStatusIcon
-                                        status={searchResult.status}
-                                    />
-                                    {
-                                        // TODO: figure out where the C & O comes from and add here (?)
-                                    }
-                                </StatusImageWrapper>
-                                <SearchResultDetailsWrapper>
-                                    <SearchResultHeaderWrapper>
-                                        <h6>{searchResult.mcPkgNo}</h6>
-                                        <p>{searchResult.commPkgNo}</p>
-                                        <p>{searchResult.responsibleCode}</p>
-                                    </SearchResultHeaderWrapper>
-                                    <p>{searchResult.description}</p>
-                                    <p>{searchResult.phaseCode} bad</p>
-                                </SearchResultDetailsWrapper>
-                            </SearchResult>
-                        );
-                    }
-                })}
-            </div>
-        );
-    };
+    console.log(searchStatus.valueOf());
 
     if (searchStatus === SearchStatus.LOADING) {
         return <SkeletonLoadingPage fullWidth />;
     }
     if (searchStatus === SearchStatus.SUCCESS && searchResults.length > 0) {
-        return determineSearchResultType(); // TODO: determine if the content of the function should just be moved here
+        searchResults.map((searchResult) => (
+            <SearchResult
+                key={searchResult.id}
+                searchResult={searchResult}
+                searchType={searchType}
+            />
+        ));
     }
     if (searchStatus === SearchStatus.INACTIVE) {
         return (
