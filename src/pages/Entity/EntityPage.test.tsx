67398bf149dedc14525b69921afd8e4c437dbb93
VRiveland <vriv@equinor.com>
2021-05-20 12:45:49 +0200

    Fixed old routing test and added the other routing test in EntityPage test file

--- src/pages/Entity/EntityPage.test.tsx
+++ src/pages/Entity/EntityPage.test.tsx
@@ -1,7 +1,7 @@
 import { withPlantContext } from '../../test/contexts';
 import EntityPage from './EntityPage';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { rest } from 'msw';
 import { server, ENDPOINTS, causeApiError } from '../../test/setupServer';
 import { SearchType } from '../Search/Search';
@@ -13,17 +13,21 @@ import {
     testScope,
 } from '../../test/dummyData';
 
-const renderScope = (searchType: SearchType): void => {
+const renderEntityPage = (
+    searchType: SearchType,
+    punchList?: boolean
+): void => {
     render(
         withPlantContext({
             Component: (
                 <MemoryRouter
-                    initialEntries={[`/plant/project/${searchType}/33`]}
+                    initialEntries={[
+                        `/plant/project/${searchType}/33${
+                            punchList ? '/punch-list' : ''
+                        }`,
+                    ]}
                 >
                     <Route path="/:plant/:project/:searchType/:itemId">
-                        <EntityPage />
-                    </Route>
-                    <Route path="/:plant/:project/:searchType/:itemId/punch-list">
                         <EntityPage />
                     </Route>
                 </MemoryRouter>
@@ -35,7 +39,7 @@ const renderScope = (searchType: SearchType): void => {
 describe('<EntityPage> general and Scope component', () => {
     it('Shows an error message in Scope component, footer card if getMcScope API call fails', async () => {
         causeApiError(ENDPOINTS.getMcScope, 'get');
-        renderScope(SearchType.MC);
+        renderEntityPage(SearchType.MC);
         expect(
             await screen.findByText('Unable to load scope. Please try again.')
         ).toBeInTheDocument();
@@ -48,7 +52,7 @@ describe('<EntityPage> general and Scope component', () => {
     });
     it('Shows an error message in MC pkg details card if getMcPkgDetails API call fails', async () => {
         causeApiError(ENDPOINTS.getMcPkgDetails, 'get');
-        renderScope(SearchType.MC);
+        renderEntityPage(SearchType.MC);
         expect(
             await screen.findByText('Unable to load details. Please reload')
         ).toBeInTheDocument();
@@ -61,7 +65,7 @@ describe('<EntityPage> general and Scope component', () => {
     });
     it('Shows an error message in footer if getMcPunchList API call fails', async () => {
         causeApiError(ENDPOINTS.getMcPunchList, 'get');
-        renderScope(SearchType.MC);
+        renderEntityPage(SearchType.MC);
         expect(
             screen.queryByText('Unable to load scope. Please try again.')
         ).not.toBeInTheDocument();
@@ -82,12 +86,12 @@ describe('<EntityPage> general and Scope component', () => {
                 );
             })
         );
-        renderScope(SearchType.MC);
+        renderEntityPage(SearchType.MC);
         expect(await screen.findByTestId('skeleton-row')).toBeInTheDocument();
         expect(await screen.findByText(testScope[0].tagNo)).toBeInTheDocument();
     });
     it('Renders the Scope component, footer, and MC pkg details card if API calls are successfull', async () => {
-        renderScope(SearchType.MC);
+        renderEntityPage(SearchType.MC);
         expect(await screen.findByText(testScope[0].tagNo)).toBeInTheDocument();
         expect(
             await screen.findByRole('button', {
@@ -104,7 +108,7 @@ describe('<EntityPage> general and Scope component', () => {
                 return response(context.json([]), context.status(200));
             })
         );
-        renderScope(SearchType.MC);
+        renderEntityPage(SearchType.MC);
         expect(
             await screen.findByText('The scope is empty.')
         ).toBeInTheDocument();
@@ -116,9 +120,7 @@ describe('<EntityPage> general and Scope component', () => {
 
 describe('<EntityPage> in-page routing', () => {
     it('Renders the PunchList component if the punch list button is clicked', async () => {
-        // This test sometimes fails when using the jest extension, but doesn't when using yarn test
-        // ^is most likely caused by the botttom expect being called too quickly (?) might be fixed once the test is completed
-        renderScope(SearchType.MC);
+        renderEntityPage(SearchType.MC);
         expect(
             await screen.findByText(testMcPkgPreview[0].mcPkgNo)
         ).toBeInTheDocument();
@@ -131,11 +133,33 @@ describe('<EntityPage> in-page routing', () => {
         expect(
             await screen.findByText(testMcPkgPreview[0].mcPkgNo)
         ).toBeInTheDocument();
-        // TODO: add an expect to test something from the punch list once the component is finished
-        // ^ will both be a better test and remove the act error
-        expect(screen.queryByText(testScope[0].tagNo)).not.toBeInTheDocument();
+        expect(
+            await screen.findByText(dummyPunchListResponse[0].tagDescription)
+        ).toBeInTheDocument();
+        await waitFor(() =>
+            expect(
+                screen.queryByText(testScope[0].tagNo)
+            ).not.toBeInTheDocument()
+        );
     });
-    it.todo('Renders Scope compoent if the scope button is clicked');
+    it('Renders Scope compoent if the scope button is clicked', async () => {
+        renderEntityPage(SearchType.MC, true);
+        expect(
+            await screen.findByText(testMcPkgPreview[0].mcPkgNo)
+        ).toBeInTheDocument();
+        expect(
+            await screen.findByText(dummyPunchListResponse[0].tagDescription)
+        ).toBeInTheDocument();
+        const scopeButton = await screen.findByRole('button', {
+            name: `Scope ${testScope.length}`,
+        });
+        expect(scopeButton).toBeInTheDocument();
+        userEvent.click(scopeButton);
+        expect(
+            await screen.findByText(testMcPkgPreview[0].mcPkgNo)
+        ).toBeInTheDocument();
+        expect(await screen.findByText(testScope[0].tagNo)).toBeInTheDocument();
+    });
 });
 
 describe('<EntityPage> punch list', () => {
