dc72d65aa3d6d3da56cadb948d7cba89440fe17a
VRiveland <vriv@equinor.com>
2021-07-13 14:46:23 +0200

    Added scope and punch list pages for Tags

--- src/pages/Entity/WorkOrderInfo.tsx
+++ src/pages/Entity/WorkOrderInfo.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable prettier/prettier */
 import { CollapsibleCard } from '@equinor/procosys-webapp-components';
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import AsyncPage from '../../components/AsyncPage';
+import ErrorPage from '../../components/error/ErrorPage';
 import { AsyncStatus } from '../../contexts/McAppContext';
-import { McPkgPreview, WoPreview } from '../../services/apiTypes';
+import { isOfType } from '../../services/apiTypeGuards';
+import { McPkgPreview, Tag, WoPreview } from '../../services/apiTypes';
 
 const TagInfoWrapper = styled.main`
     min-height: 0px;
@@ -15,7 +19,7 @@ const TagInfoWrapper = styled.main`
 `;
 
 type WorkOrderInfoProps = {
-    workOrder?: WoPreview | McPkgPreview;
+    workOrder?: WoPreview | McPkgPreview | Tag;
     fetchWorkOrderStatus: AsyncStatus;
 };
 
@@ -23,18 +27,32 @@ const WorkOrderInfo = ({
     workOrder,
     fetchWorkOrderStatus,
 }: WorkOrderInfoProps): JSX.Element => {
-    return (
-        <AsyncPage
-            fetchStatus={fetchWorkOrderStatus}
-            errorMessage={'Unable to load tag info. Please try again.'}
-        >
-            <TagInfoWrapper>
-                <CollapsibleCard cardTitle="Description">
-                    <p>{workOrder?.description}</p>
-                </CollapsibleCard>
-            </TagInfoWrapper>
-        </AsyncPage>
-    );
+    if (
+        workOrder === undefined ||
+        isOfType<WoPreview>(workOrder, 'workOrderNo')
+    ) {
+        return (
+            <AsyncPage
+                fetchStatus={fetchWorkOrderStatus}
+                errorMessage={'Unable to load Work Order info. Please try again.'}
+            >
+                <TagInfoWrapper>
+                    <CollapsibleCard cardTitle="Description">
+                        <p>
+                            {workOrder?.description}
+                        </p>
+                    </CollapsibleCard>
+                </TagInfoWrapper>
+            </AsyncPage>
+        );
+    } else {
+        return (
+            <ErrorPage
+                title="Unable to load Work Order info."
+                description="The Work Order info page is only available for Work Orders."
+            />
+        );
+    }
 };
 
 export default WorkOrderInfo;
