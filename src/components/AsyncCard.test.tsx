92d20110f2a08c3e47bbd9423c2494ec271a10bf
Erlend T Lygre <erlend.tl2@gmail.com>
2021-09-03 12:42:42 +0200

    Removed unused components and components that have been moved to library

--- src/components/AsyncCard.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-describe('<AsyncCard/>', () => {
-    test.todo('Render error message when appropriate');
-    test.todo('Renders loading state');
-    test.todo('Renders no content message');
-    test.todo('Renders children upon succes');
-});
