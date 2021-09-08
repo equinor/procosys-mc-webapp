029fe6dda69aed459f035d75b81a50dcd0d49f16
Christopher Berge Hove <chris@nogglings.com>
2021-09-08 19:50:42 +0200

    chore: updated function to be more clean and easy to read and added unit test.

--- src/utils/removeHtmlFromText.ts
+++ src/utils/removeHtmlFromText.ts
@@ -1,26 +1,7 @@
 export function removeHtmlFromText(text?: string): string {
-    if (text) {
-        return text
-            .replace(/<\/P>/g, '\n')
-            .replace(/<P>/g, '')
-            .replace(/<\/a>/g, '\n')
-            .replace(/<a>/g, '')
-            .replace(/<\/div>/g, '\n')
-            .replace(/<div>/g, '')
-            .replace(/<\/span>/g, '\n')
-            .replace(/<span>/g, '')
-            .replace(/<\/h1>/g, '\n')
-            .replace(/<h1>/g, '')
-            .replace(/<\/h2>/g, '\n')
-            .replace(/<h2>/g, '')
-            .replace(/<\/h3>/g, '\n')
-            .replace(/<h3>/g, '')
-            .replace(/<\/h4>/g, '\n')
-            .replace(/<h4>/g, '')
-            .replace(/<\/h6>/g, '\n')
-            .replace(/<h6>/g, '')
-            .replace(/<\/h6>/g, '\n')
-            .replace(/<h6>/g, '');
-    }
-    return '';
+    if (!text) return '';
+    return text
+        .replace(/<\/[^]>/gm, '\n')
+        .replace(/<[^>]>/g, ' ')
+        .trimStart();
 }
