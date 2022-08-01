a31f3981aa8ecaf0c76959ad3312ad625edc5802
Dan Halvorsen <dehalvorsen@gmail.com>
2022-08-02 00:25:32 +0200

    adding new integration test concept

---
+++ testing/integration/src/index.ts
@@ -0,0 +1,25 @@
+import { expect, should } from 'chai';
+import 'chai/register-should';
+
+
+import 'chai/register-should';
+import sinon from 'sinon';
+import fetchMock from 'fetch-mock';
+;
+
+
+// describe('Typescript usage suite', () => {
+//     it('should be able to execute a test', () => {
+//         return;
+//     });
+//     it('should return expected string', () => {
+//         return;
+//     });
+// });
+
+
+//https://sinonjs.org/
+// require("chai/register-should");
+
+//http://www.wheresrhys.co.uk/fetch-mock/
+
