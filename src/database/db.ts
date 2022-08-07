442d97c61e4d869a7f485e2158da61af1fc31cb1
Dan Halvorsen <dehalvorsen@gmail.com>
2022-08-07 22:10:28 +0200

    adding more files

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,17 +1,23 @@
 import Dexie, { Table } from 'dexie';
+import { Entity, EntityIndexes, IEntity } from './entity';
 import { IStatus, Status } from './status';
 
 export default class OfflineStorage extends Dexie {
-    public readonly dbName: string = 'offlineStorage';
     offlineStatus!: Table<IStatus>;
+    offlineContent!: Table<IEntity, EntityIndexes>;
+    static offlineContent: any;
 
     constructor() {
         super('offlineStorage');
-        this.version(1).stores({
-            offlineStatus: 'name',
-        });
+        this.version(1).stores(
+            {
+                offlineStatus: 'name',
+                offlineContent: '++id, entityid, entitytype',
+            }
+        );
 
-        this.offlineStatus.mapToClass(Status);
+        this!.offlineStatus.mapToClass(Status);
+        this!.offlineContent.mapToClass(Entity);
     }
 }
 
---
+++ src/database/entity.ts
@@ -0,0 +1,36 @@
+import Dexie, { Table } from 'dexie';
+
+export interface IEntity {
+    entityid: number;
+    entitytype: string;
+    // response: Response
+    // request: Request
+    data: string;
+}
+
+export class Entity implements IEntity {
+    entityid!: number;
+    entitytype!: string;
+    data!: string;
+    // response!: Response;
+    // request!: Request;
+}
+
+export type EntityIndexes = Pick<IEntity, "entityid" | "entitytype">;
+
+import db from "./db";
+export class EntityRepository {
+
+
+
+    async getById(id: number): Promise<Entity> {
+        let result = await db.offlineContent.where('id').equals(id).first();
+        return result as Entity;
+    }
+
+    async getByEntityId(entityId: number): Promise<Entity> {
+        const result = await db.offlineContent.where('id').equals(entityId).first();
+        return result as Entity;
+    }
+
+}
