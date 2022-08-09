a3cc1e35fab8cc224877a5d80d71d24e406776c5
Dan Edvard Halvorsen <dan.halvorsen@bouvet.no>
2022-08-09 08:58:24 +0200

    fixing building errors

--- testing/integration/src/specs/database-testing-out-specs.ts
+++ testing/integration/src/specs/database-testing-out-specs.ts
@@ -8,7 +8,7 @@ import { IEntity } from '../../../../src/database/IEntity';
 import { EntityIndexes } from '../../../../src/database/EntityIndexes';
 import { deleteDBs } from './delete-dbs';
 
-describe('Given a fetchAPI', async () => {
+describe('Given a Entity database repository', async () => {
     const backupProgress = (progress: ExportProgress): boolean => {
         console.log('progress:', progress.completedRows);
         return true;
@@ -39,10 +39,12 @@ describe('Given a fetchAPI', async () => {
         // await db.import(dbBackup);
     });
 
-    it('should add a new entity record in database', async () => {
+    const testEnityId = 12;
+
+    it('should be able to add a new Entity record into database', async () => {
         const reps = new EntityRepository();
         const entity: IEntity = {
-            entityid: 12,
+            entityid: testEnityId,
             entitytype: 'test',
             data: 'datadatadata',
         };
@@ -51,4 +53,25 @@ describe('Given a fetchAPI', async () => {
 
         expect(seqId).to.be.greaterThan(0);
     });
+
+    it('should be able find a record in database', async () => {
+        const reps = new EntityRepository();
+        const searchingForEntity = await reps.find(testEnityId);
+        expect(searchingForEntity).to.not.be.undefined;
+        if (searchingForEntity !== undefined) {
+            expect(searchingForEntity.entityid).to.be.equal(testEnityId);
+        }
+    });
+
+    it('should be able to delete a record in database', async () => {
+        const reps = new EntityRepository();
+        const deletedId = await reps.delete(testEnityId);
+        expect(deletedId).to.be.equal(1);
+        const searchResult = await reps.find(testEnityId);
+        expect(searchResult).to.be.undefined;
+    });
 });
+
+/*
+Hi my name is: Dan and I have a family of people that are members of the database
+*/
--- testing/integration/src/specs/delete-dbs.ts
+++ testing/integration/src/specs/delete-dbs.ts
@@ -1,16 +1,19 @@
-import { Dexie } from "dexie";
+import { Dexie } from 'dexie';
 
-
-export const deleteDBs = (names: string[]) => Dexie.getDatabaseNames((names) => {
-    console.log('database names: ', names);
-    names.forEach(function (name) {
-        var db = new Dexie(name);
-        db.delete().then(function () {
-            console.log('Database successfully deleted: ', name);
-        }).catch(function (err) {
-            console.error('Could not delete database: ', name, err);
-        }).finally(function () {
-            console.log('Done. Now executing callback if passed.');
+export const deleteDBs = (names: string[]) =>
+    Dexie.getDatabaseNames((names) => {
+        console.log('database names: ', names);
+        names.forEach(function (name) {
+            const db = new Dexie(name);
+            db.delete()
+                .then(function () {
+                    console.log('Database successfully deleted: ', name);
+                })
+                .catch(function (err) {
+                    console.error('Could not delete database: ', name, err);
+                })
+                .finally(function () {
+                    console.log('Done. Now executing callback if passed.');
+                });
         });
     });
-});
