00c15ebda3b896a521e8b714851a3957a78271ec
VRiveland <vriv@equinor.com>
2022-11-14 12:53:35 +0100

    Added more console.logs

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -26,13 +26,17 @@ type Config = {
 };
 
 export function register(config?: Config): void {
+    console.log('register', process.env.NODE_ENV);
+    console.log('register', process.env.PUBLIC_URL);
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+        console.log('has started registering');
         // The URL constructor is available in all browsers that support SW.
         const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
         if (publicUrl.origin !== window.location.origin) {
             // Our service worker won't work if PUBLIC_URL is on a different origin
             // from what our page is served on. This might happen if a CDN is used to
             // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+            console.log('service worker wrong origin');
             return;
         }
 
@@ -40,6 +44,7 @@ export function register(config?: Config): void {
             const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
             if (isLocalhost) {
+                console.log('is localhosts');
                 // This is running on localhost. Let's check if a service worker still exists or not.
                 checkValidServiceWorker(swUrl, config);
 
