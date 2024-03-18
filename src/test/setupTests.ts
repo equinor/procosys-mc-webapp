df798484b0cf413fe56bf9db1af836125c7aeacd
Christian Alejandro Hormazabal Martin <chorm@equinor.com>
2024-03-18 14:31:57 +0100

    upgrade styled components

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import '@equinor/eds-core-react';
 import './setupServer';
 import { configure } from '@testing-library/react';
