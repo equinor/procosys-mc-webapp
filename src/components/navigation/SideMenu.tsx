cd4d9ec9cfaa10b5ad1354d9448c6586d8ed6647
sondreravndal <sondre.pedersen@bouvet.no>
2024-06-14 11:17:31 +0200

    updated versions

--- src/components/navigation/SideMenu.tsx
+++ src/components/navigation/SideMenu.tsx
@@ -192,6 +192,12 @@ const SideMenu = ({ isOffline = false }: SideMenuProps): JSX.Element => {
                     <p>
                         Versions <br />
                         MC App: {packageJson.version} <br />
+                        MC Webapp components:{' '}
+                        {
+                            packageJson.dependencies[
+                                '@equinor/procosys-webapp-components'
+                            ]
+                        }
                     </p>
                     {localStorage.getItem(LocalStorage.SW_UPDATE) == 'true' ? (
                         <Button onClick={updateServiceWorker}>
